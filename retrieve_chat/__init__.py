7677cf886c8b2a620d5ca9f2b5b91721f69f58f6
sethjuarez <me@sethjuarez.com>
2020-12-04 14:02:39 -0800

    added downloader code and added sample call in func

--- retrieve_chat/__init__.py
+++ retrieve_chat/__init__.py
@@ -1,24 +1,23 @@
 import logging
-
 import azure.functions as func
+from ..shared.chat_replay_downloader import *
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    post = req.get_json()
+    logging.info(post['video'])
+    # get messages via url
+    youtube_messages = get_chat_replay('https://www.youtube.com/watch?v=xxxxxxxxxxx')
+    twitch_messages = get_chat_replay('https://www.twitch.tv/videos/xxxxxxxxx')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    # get messages via id
+    youtube_messages = get_youtube_messages('xxxxxxxxxxx')
+    twitch_messages = get_twitch_messages('xxxxxxxxx')
+
+    
+    return func.HttpResponse(
+        "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+        status_code=200
+    )
